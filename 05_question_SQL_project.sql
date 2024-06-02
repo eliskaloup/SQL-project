44fc72d7e6edf7b57712e7215f06fc90c9c62bdf
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-02 21:16:35 +0200

    Update 05_question_SQL_project.sql

--- 05_question_SQL_project.sql
+++ 05_question_SQL_project.sql
@@ -1,22 +1,22 @@
 --otazka_5: Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
 
 --HDP rostlo v roce 2007, 2015, 2017
-	--kleslo 2009, 2012, 2013
+    --kleslo 2009, 2012, 2013
 
---(beru tedy v potaz rok kdy nastavala zmena a i ten nasledujici)
+--(beru tedy v potaz rok kdy nastavala zmena hdp a i nasledujici rok)
 
 
---(+)V roce 2006 a 2007 byl extremni narust cen u vice jak 20 druhu potravin. (narust az 66%)
---(-+) V roce 2015- 2016 byly velke vykyvy, spise se hladina udrzovala kolem nuly. Nebylo zadne velke prevyseni oproti zbylych dvou let (2007 a 2017)
---(-)V roce 2017 a 2018 byl extremni pokles cen, a TO u vice jak 15 druhu potravin. (pokles az 31%)
+--(+) V roce 2006 a 2007 byl extremni narust cen u vice jak 20 druhu potravin (narust az 66 % ).
+--(-+) V roce 2015 a 2016 byly velke vykyvy, spise se hladina udrzovala kolem nuly.
+--(-) V roce 2017 a 2018 byl extremni pokles cen, a to u vice jak 15 druhu potravin (pokles az 31 % ).
 
 --(+)V letech co HDP rostlo jsme zaznamenali pouze par poklesu ve mzdach, takze urcite jde videt ze HDP toto ovlivlino.
---(-)Nejvice ovlivnil pokles HDP rok 2012, protoze v roce 2013 bylo ovlivnen pokles mezd u 11 odvetvi z celkovych 19.
+--(-)Nejvice ovlivnil pokles HDP rok 2012, protoze v roce 2013 byl ovlivnen pokles mezd u 11 odvetvi z celkovych 19.
 
 ---Finalni shrnuti:
-	--Kdyz v letech 2007, 2015, 2017 HDP melo nejvetsi procentualni narust tak se mzdy ve velike prevaze zvysovaly. 
-	--Co se tyce cen potravin,tak jsme zaznamenali ze v roce 2006 a 2007 extremni narust cen, TO je uplne opacny pripad roku 2017 a 2018 kdy byl extremni pokles cen,
-	--Co se tyce let 2015 a 2016 tak urcite velke vykyvy nastaly, ale spise se drzi kolem 0 a +- 15 %
+	--Kdyz v letech 2007, 2015, 2017 HDP melo nejvetsi procentualni narust tak se mzdy ve velke prevaze zvysovaly. 
+	--Co se tyce cen potravin, tak jsme zaznamenali v roce 2006 a 2007 extremni narust cen, to je uplne opacny pripad roku 2017 a 2018 kdy byl extremni pokles cen.
+	--V letech 2015 a 2016 take nastaly vykyvy cen, ale hladina se spise drzi kolem 0 a +- 15 %. Nebylo zadne velke prevyseni jako jsme vypozorovali v let 2007 a 2017.
 
 --(Co se tyce tabulky ohledne poklesu mezd, tak jsem vychazela z excel tabulky 01_question_excel_screenshot)
 	
@@ -32,10 +32,6 @@ GROUP BY pf.payroll_year
 ORDER BY pf.payroll_year 
 
 
-SELECT *
-FROM v_fifth_question AS vf 
-
-
 SELECT *,
 	CASE 
 		WHEN gdp_percent > 5 THEN 'HIGH gdp growth'
@@ -47,7 +43,7 @@ ORDER BY gdp_percent DESC
 
 
 --tabulka pouze kategorii produktu.
---tabulka je skoro totozna jako druhe otazky ,pouze bez pouziti prvniho a posledniho roku.
+--tabulka je skoro totozna jako u druhe otazky, pouze bez pouziti min a max roku ale vsech let.
 
 CREATE OR REPLACE VIEW v_fifth_question_products AS
 SELECT pf.categ_code,
