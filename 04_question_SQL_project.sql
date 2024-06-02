65b1d4ecc8511d81c8be80fb833135f05d45620b
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-02 19:52:14 +0200

    Update 04_question_SQL_project.sql

--- 04_question_SQL_project.sql
+++ 04_question_SQL_project.sql
@@ -8,18 +8,13 @@ SELECT
 	pf.payroll_year,
 	pf.avg_price_value AS average_price,
 	pf.avg_payroll_value AS average_salary,
-	pf2.payroll_year + 1 AS year_prev,
 	round( ( avg(pf.avg_price_value) - avg(pf2.avg_price_value) ) / avg(pf2.avg_price_value) * 100, 2 ) AS price_growth,
 	round( ( avg(pf.avg_payroll_value) - avg(pf2.avg_payroll_value) ) / avg(pf2.avg_payroll_value) * 100, 2) AS payroll_percent
-FROM
-	t_eliska_loup_project_sql_primary_final AS pf
+FROM t_eliska_loup_project_sql_primary_final AS pf
 JOIN t_eliska_loup_project_sql_primary_final AS pf2
-    ON
-	pf.payroll_year = pf2.payroll_year + 1
-GROUP BY
-	payroll_year
-ORDER BY
-	payroll_year;
+    ON pf.payroll_year = pf2.payroll_year + 1
+GROUP BY payroll_year
+ORDER BY payroll_year;
 
 
 SELECT *,
