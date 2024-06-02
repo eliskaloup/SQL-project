aad5cb4534db346c68b1a813b1deb87e9f528817
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-02 20:25:11 +0200

    Update 01_01_engeto_SQL_project_primary_final.sql

--- 01_01_engeto_SQL_project_primary_final.sql
+++ 01_01_engeto_SQL_project_primary_final.sql
@@ -1,4 +1,5 @@
---primarni tabulka
+-- primarni tabulka
+
 CREATE OR REPLACE TABLE t_eliska_loup_project_SQL_primary_final AS 
 SELECT czpay.industry_branch_code AS ib_code,
 	ind_br.name AS ind_name,
@@ -31,37 +32,37 @@ ORDER BY ib_code, payroll_year, categ_code, czp.date_from;
 SELECT *
 FROM t_eliska_loup_project_sql_primary_final AS telpspf;
 
----postup jak jsem k ni dosla
---kde jsou NULL hodnoty?
+--- muj postup
+
+-- kde jsou NULL hodnoty?
 SELECT *
 FROM czechia_payroll AS cp   
 WHERE cp.industry_branch_code IS NULL;
 
-
---z jakych let mame data?
+-- z jakych let mame data?
 SELECT min(cp.payroll_year),
 	max(cp.payroll_year)
 FROM czechia_payroll AS cp;
 
---kontrola zda jsou roky zapsany stejne, ne v jinem tvaru
+-- kontrola zda jsou roky zapsany stejne, ne v jinem tvaru
 SELECT DISTINCT payroll_year, payroll_quarter  
 FROM czechia_payroll AS cp 
 ORDER BY payroll_year DESC, payroll_quarter;
 
---czechia_price: je v date_from a date_to nějaká hodnota nulová?
+-- czechia_price: je v date_from a date_to nějaká hodnota nulová?
 SELECT *
 FROM czechia_price  
 WHERE date_from IS NULL
 	OR date_to IS NULL; --žádná hodnota není nulová 
 
---z jakych let mame vsechny DATA?	
+-- z jakych let mame vsechny data?	
 SELECT min(cp.date_from),
 	max(cp.date_from),
 	min(cp.date_to),
 	max(cp.date_to)
 FROM czechia_price AS cp;
---data mame z let 2006 az 2018
+-- data mame z let 2006 az 2018
 
---jelikož je datový typ definován datetime nemůže zde být nevyhovující hodnota
---tady i zjistim, je k dispozici měření za celý rok 2006 (min(cp.date_from)= 2.1.2006) a za celý rok 2018 (max(cp.date_to)=16.12.2018)
+-- jelikož je datový typ definován datetime nemůže zde být nevyhovující hodnota
+-- tady i zjistim, je k dispozici měření za celý rok 2006 (min(cp.date_from)= 2.1.2006) a za celý rok 2018 (max(cp.date_to)=16.12.2018)
 
