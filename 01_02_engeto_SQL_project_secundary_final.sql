83da4249ebe5b27a812623d511b9378647461011
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-02 21:20:10 +0200

    Update 01_02_engeto_SQL_project_secundary_final.sql

--- 01_02_engeto_SQL_project_secundary_final.sql
+++ 01_02_engeto_SQL_project_secundary_final.sql
@@ -11,11 +11,11 @@ JOIN countries AS c
 	ON c.country = e.country
 	WHERE c.continent = 'Europe'
 	AND e.year BETWEEN 2006 AND 2018
-ORDER BY c.country, e.year 
+ORDER BY c.country, e.year;
 
 
 SELECT *
-FROM t_eliska_loup_project_SQL_secondary_final AS sf
+FROM t_eliska_loup_project_SQL_secondary_final AS sf;
 
 -- Tuto tabulku jsem nakonec ani nepouzila, vychazela jsem z primary, kde jsem vyfiltrovala pouze Cesko, prijde mi zbytecne vychazet z teto tabulky,
 -- protoze kdyz HDP je ovlivneno v Evrope tak je i v Cesku. A nebylo tedy receno konkretne, zda se 5. otazka vztahuje z hlediska na EU nebo CZ.
