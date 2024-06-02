3de909dd657feb490b378e41fe03f0ea182ce59d
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-02 20:14:19 +0200

    Update 01_question_SQL_project.sql

--- 01_question_SQL_project.sql
+++ 01_question_SQL_project.sql
@@ -8,22 +8,17 @@
 
 
 CREATE OR REPLACE VIEW v_first_quest AS 
-SELECT 
-	ib_code,
+SELECT 	ib_code,
 	ind_name,
 	payroll_year,
 	avg_payroll_value AS average_salary,
 	LAG(avg_payroll_value) OVER (PARTITION BY ind_name ORDER BY payroll_year) AS previous_year_salary
 FROM t_eliska_loup_project_sql_primary_final AS pf
-GROUP BY 
-	ind_name,
-	payroll_year 
-ORDER BY
-	ind_name,	
-	payroll_year;
+GROUP BY  ind_name, payroll_year 
+ORDER BY ind_name, payroll_year;
 	
 SELECT *,
-		CASE
+	CASE
 		WHEN previous_year_salary < average_salary THEN 'salary increased'
 		ELSE 'salary DECREASED'
 	END AS payroll_growth
