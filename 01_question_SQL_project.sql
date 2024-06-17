e57306f38a037cc55d26a6abb8b36639d3d7e86c
eliskaloup <161161824+eliskaloup@users.noreply.github.com>
2024-06-18 01:06:28 +0200

    Update 01_question_SQL_project.sql

--- 01_question_SQL_project.sql
+++ 01_question_SQL_project.sql
@@ -1,12 +1,5 @@
 -- Otazka_1: Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
--- Ve vetsine odvetvi mzdy obcas klesly, pouze ve 3 odvetvich nikdy neklesly: 
--- Mzdy se vzdy zvysovaly pouze ve 3 odvetvich, a to v: Zpracovatelskem prumyslu, Zdravotnictvi a Ostatnich cinnostech.
-
--- Nejvice klesly mzdy v Tezebnictvi- 4 poklesy v letech 2009, 2013, 2014 a 2016.
--- A druhy nejvetsi pokles byl u Vyroby a rozvodu elektriny- 3 poklesy v letech 2011, 2013 a 2015.
-
-
 CREATE OR REPLACE VIEW v_first_quest AS 
 SELECT 	ib_code,
 	ind_name,
